0a242c8da14d436b7e3ab08ab6b70b87e1835021
Michael Major <majormw@outlook.com>
2023-02-27 18:47:28 -0500

    new file

---
+++ bash/intnametest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+interface=$(ifconfig -s | awk '{print$1}')
+
+for interface in $interface; do
+  echo "Interface name: $interface";
+  sleep 3;
+  if $interface = "Iface";then
+	echo "skipping Iface";
+  fi
+done
