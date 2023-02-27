58fdedd7f79f207aec66973c5bac294a15a35f98
KripsyKing <122486908+KripsyKing@users.noreply.github.com>
2023-02-27 18:53:56 -0500

    Update intnametest.sh

--- bash/intnametest.sh
+++ bash/intnametest.sh
@@ -3,8 +3,9 @@
 interface=$(ifconfig -s | awk '{print$1}')
 
 for interface in $interface; do
-  sleep 1;
   if [ $interface="Iface" ];then
 	echo "skipping Iface";
   fi
+  echo "Interface name: $interface";
+  sleep 1;
 done
