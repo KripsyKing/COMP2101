fe542e3d82ba405830287701b9cf5b232eea0f2a
KripsyKing <mwmajor@icloud.com>
2023-06-21 20:04:59 -0400

    update

--- bash/pratice scripts/testdiskreport.sh
+++ bash/pratice scripts/testdiskreport.sh
@@ -9,11 +9,11 @@ diskreport() {
     echo "------------------------------------------------------------------------------------------------"
     local disk_info=$(lsblk -o NAME,SIZE,VENDOR,MODEL | grep -v "loop" | grep -v "sr0")
     while IFS= read -r line; do
-        local disk_name=$(echo "$line" | awk '{print $1}')
-        local disk_size=$(echo "$line" | awk '{print $2}')
-        local disk_vendor=$(echo "$line" | awk '{print $3}')
-        local disk_model=$(echo "$line" | awk '{print $4}')
-        local partition_info=$(lsblk -o NAME,MOUNTPOINT,FSTYPE,SIZE -n -r "/dev/$disk_name" 2>/dev/null)
+#        local disk_name=$(echo "$line" | awk '{print $1}')
+#        local disk_size=$(echo "$line" | awk '{print $2}')
+#        local disk_vendor=$(echo "$line" | awk '{print $3}')
+#        local disk_model=$(echo "$line" | awk '{print $4}')
+#        local partition_info=$(lsblk -o NAME,MOUNTPOINT,FSTYPE,SIZE -n -r "/dev/$disk_name" 2>/dev/null)
         while IFS= read -r partition_line; do
             local partition_name=$(echo "$partition_line" | awk '{print $1}')
             local mount_point=$(echo "$partition_line" | awk '{print $2}')
@@ -25,3 +25,5 @@ diskreport() {
     done <<< "$disk_info"
     echo "------------------------------------------------------------------------------------------------"
 }
+
+diskreport
