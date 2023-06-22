cf2d34cf45affbe489cdfa001221348165686e5d
KripsyKing <majormw@outlook.com>
2023-06-21 20:18:24 -0400

    update

--- bash/pratice scripts/testdiskreport.sh
+++ bash/pratice scripts/testdiskreport.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
-function diskreport {
-  # Print title
-  echo "Disk Report"
-
-  # Print table headers
-  printf "%-20s %-20s %-20s %-20s %-20s %-20s %-20s\n" "Manufacturer" "Model" "Size" "Partition" "Mount Point" "Filesystem Size" "Free Space"
-
-  # Iterate over each disk drive
-  for drive in $(lsblk -ndo NAME,MODEL,SIZE | grep -v "loop"); do
-    name=$(echo "$drive" | awk '{print $1}')
-    model=$(echo "$drive" | awk '{print $2}')
-    size=$(echo "$drive" | awk '{print $3}')
-    partitions=$(lsblk -ndo NAME | grep "^$name")
-    
-    # Iterate over each partition of the drive
-    for partition in $partitions; do
-      mountpoint=$(lsblk -ndo MOUNTPOINT | grep "^/.*$partition$")
-      filesystem_size=""
-      free_space=""
-      
-      # Check if the partition is mounted
-      if [[ -n $mountpoint ]]; then
-        filesystem_size=$(df -h | grep "$partition" | awk '{print $2}')
-        free_space=$(df -h | grep "$partition" | awk '{print $4}')
-      fi
-      
-      # Print the information in the table format
-      printf "%-20s %-20s %-20s %-20s %-20s %-20s %-20s\n" "$name" "$model" "$size" "$partition" "$mountpoint" "$filesystem_size" "$free_space"
-    done
-  done
+# Function to convert bytes to human-friendly format
+human_readable() {
+    awk -v bytes="${1}" 'BEGIN {
+        units["B"] = 0;
+        units["KB"] = 1;
+        units["MB"] = 2;
+        units["GB"] = 3;
+        units["TB"] = 4;
+        unit = "B";
+        size = bytes;
+        for (i = 0; size >= 1024 && i < 4; i++) {
+            size = size / 1024;
+            unit = "KB MB GB TB"[i + 1];
+        }
+        printf "%.2f %s", size, unit;
+    }'
 }
 
-# Call the diskreport function
-diskreport
+# Get the disk drive information using 'lsblk' command
+drive_info=$(lsblk -b -o NAME,TYPE,MODEL,SIZE,MOUNTPOINT,FSTYPE,FSSIZE,FSUSED,FSAVAIL | tail -n +2)
+
+# Print the report title
+echo "Disk Drive Report"
+echo "-----------------"
+echo
+
+# Print the table header
+printf "%-10s %-10s %-20s %-15s %-10s %-15s %-15s\n" "Drive" "Type" "Model" "Size" "Partition" "Mount Point" "Free Space"
+echo "------------------------------------------------------------------------------------"
+
+# Loop through the drive information and print the table rows
+while read -r drive type model size mountpoint fstype fssize fsused fsavail; do
+    # Convert the drive size to a human-friendly format
+    size_human=$(human_readable "$size")
+
+    # Check if the drive is mounted
+    if [[ -n "$mountpoint" ]]; then
+        # Convert the filesystem size and free space to human-friendly formats
+        fssize_human=$(human_readable "$fssize")
+        fsavail_human=$(human_readable "$fsavail")
+
+        # Print the mounted drive information
+        printf "%-10s %-10s %-20s %-15s %-10s %-15s %-15s\n" "$drive" "$type" "$model" "$size_human" "$mountpoint" "$fssize_human" "$fsavail_human"
+    else
+        # Print the unmounted drive information
+        printf "%-10s %-10s %-20s %-15s %-10s %-15s %-15s\n" "$drive" "$type" "$model" "$size_human" "-" "-" "-"
+    fi
+done <<< "$drive_info"
