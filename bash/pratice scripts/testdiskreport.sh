1dfe30cf33fb8e33c72cb735eb840a4b2eac617e
KripsyKing <mwmajor@icloud.com>
2023-06-28 18:10:49 -0400

    file change

--- bash/pratice scripts/testdiskreport.sh
+++ bash/pratice scripts/testdiskreport.sh
@@ -5,8 +5,8 @@ diskreport() {
     echo " "
     echo "Disk Report"
     echo "-----------"
-    echo "Manufacturer    Model        Size    Partition    Mount Point    Filesystem Size    Free Space"
-    echo "------------------------------------------------------------------------------------------------"
+#    echo "Manufacturer    Model        Size    Partition    Mount Point    Filesystem Size    Free Space"
+#    echo "------------------------------------------------------------------------------------------------"
     local disk_info=$(lsblk -o NAME,SIZE,VENDOR,MODEL | grep -v "loop" | grep -v "sr0")
     while IFS= read -r line; do
         local disk_name=$(echo "$line" | awk '{print $1}')
@@ -14,14 +14,7 @@ diskreport() {
         local disk_vendor=$(echo "$line" | awk '{print $3}')
         local disk_model=$(echo "$line" | awk '{print $4}')
         local partition_info=$(lsblk -o NAME,MOUNTPOINT,FSTYPE,SIZE -n -r "/dev/$disk_name" 2>/dev/null)
-        while IFS= read -r partition_line; do
-            local partition_name=$(echo "$partition_line" | awk '{print $1}')
-            local mount_point=$(echo "$partition_line" | awk '{print $2}')
-            local filesystem_type=$(echo "$partition_line" | awk '{print $3}')
-            local partition_size=$(echo "$partition_line" | awk '{print $4}')
-            local partition_free_space=$(df -h --output=avail "/dev/$partition_name" 2>/dev/null | tail -n 1)
-            echo "$disk_vendor    $disk_model    $disk_size    $partition_name    $mount_point    $filesystem_type    $partition_size    $partition_free_space"
-        done <<< "$partition_info"
+        echo "$disk_size    $disk_vendor    $disk_model    $partition_name    $mount_point    $filesystem_type    $partition_size    $partition_free_space"
     done <<< "$disk_info"
     echo "------------------------------------------------------------------------------------------------"
 }
