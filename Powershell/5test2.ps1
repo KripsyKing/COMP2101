4ac8e580498f43927dd819ac3f1eb059d1e4c6b0
KripsyKing <122486908+KripsyKing@users.noreply.github.com>
2023-08-09 14:39:23 -0400

    Update 5test2.ps1

--- Powershell/5test2.ps1
+++ Powershell/5test2.ps1
@@ -18,20 +18,47 @@ function Get-SystemInfo {
         Video = "$($videoInfo.Name) $($videoInfo.AdapterRAM / 1MB) MB"
     }
 }
+#Getting Disk Information
+function Get-DiskInfo {
+    $diskInfo = Get-CIMInstance CIM_diskdrive
 
-function Get-DisksInfo {
-    $disksInfo = Get-WmiObject Win32_DiskDrive | Select-Object Model, Size
+    $diskDrive = @()
 
-    $disksInfo | ForEach-Object {
-        [PSCustomObject]@{
-            Disk = $_.Model
-            Size = "$($_.Size / 1GB) GB"
+    foreach ($disk in $diskInfo) {
+        $partitions = $disk | Get-CimAssociatedInstance -ResultClassName CIM_diskpartition
+
+        foreach ($partition in $partitions) {
+            $logicalDisks = $partition | Get-CimAssociatedInstance -ResultClassName CIM_LogicalDisk
+
+            foreach ($logicalDisk in $logicalDisks) {
+                $diskDrive += [PSCustomObject]@{
+                    Manufacturer = $disk.Manufacturer
+                    Location = $partition.DeviceID
+                    Drive = $logicalDisk.DeviceID
+                    SizeGB = [math]::Round($logicalDisk.Size / 1GB, 2)
+                    FreeSpaceGB = [math]::Round($logicalDisk.FreeSpace / 1GB, 2)
+                    PercentageFree = [math]::Round(($logicalDisk.FreeSpace / $logicalDisk.Size) * 100, 2)
+                }
+            }
         }
     }
+
+    return $diskDrive
 }
+#function Get-DisksInfo {
+    #$disksInfo = Get-WmiObject Win32_DiskDrive | Select-Object Model, Size
+
+    #$disksInfo | ForEach-Object {
+     #   [PSCustomObject]@{
+    #        Disk = $_.Model
+   #         Size = "$($_.Size / 1GB) GB"
+  #      }
+ #   }
+#}
 
 function Get-NetworkInfo {
-    $networkInfo = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | Select-Object Description, IPAddress
+    $networkInfo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled }
+    #Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | Select-Object Description, IPAddress
 
     $networkInfo | ForEach-Object {
         [PSCustomObject]@{
